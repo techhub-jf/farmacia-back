7ce24665e6c31be1d2731d7546bca7470007e4b2
Gustavo Garcia <gst.garc@gmail.com>
2024-06-20 15:46:00 -0300

    Create client entity

---
+++ app/domain/entity/client.go
@@ -0,0 +1,22 @@
+package entity
+
+import "time"
+
+type Client struct {
+	ID 					uint                `json:"id"`
+    Reference 			string              `json:"reference"`
+    Full_name 			string              `json:"full_name"`
+    Birth 				time.Time           `json:"birth"`
+    Cpf 				string              `json:"cpf"`
+    Rg 					string              `json:"rg"`
+    Phone 				string              `json:"phone"`
+    Cep 				string              `json:"cep"`
+    Address 			string              `json:"address"`
+    Address_number 		string              `json:"address_number"` 
+    District 			string              `json:"district"`
+    City 				string              `json:"city"`
+    State 				string              `json:"state"`
+    CreatedAt 			time.Time           `json:"created_at"`
+    UpdatedAt 			time.Time           `json:"updated_at"`
+    DeletedAt 			time.Time           `json:"deleted_at"`
+}

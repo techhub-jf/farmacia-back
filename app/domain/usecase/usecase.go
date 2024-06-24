636b5f11bf0440652e1847fabb0699b0f2cc2ff6
Lucas de Castro <lucascastrofr@gmail.com>
2024-06-23 23:38:38 -0300

    feat(delivery): add repository to get all

--- app/domain/usecase/usecase.go
+++ app/domain/usecase/usecase.go
@@ -4,14 +4,20 @@ import (
 	"context"
 
 	"github.com/techhub-jf/farmacia-back/app/domain/entity"
+	"github.com/techhub-jf/farmacia-back/app/gateway/postgres/repositories"
 )
 
 type UseCase struct {
 	AppName string
 
-	AccountsRepository accountsRepository
+	AccountsRepository   accountsRepository
+	DeliveriesRepository deliveriesRepository
 }
 
 type accountsRepository interface {
 	GetAccountByEmail(ctx context.Context, email string) (entity.Account, error)
+}
+
+type deliveriesRepository interface {
+	GetAll(ctx context.Context, filters repositories.PaginationFilters) ([]*entity.Delivery, error)
 }
---
+++ app/gateway/postgres/repositories/deliveries_get.go
@@ -0,0 +1,75 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/techhub-jf/farmacia-back/app/domain/entity"
+)
+
+const defaultLimit = 10
+
+type PaginationFilters struct {
+	Page      int32
+	Sort_by   string
+	Sort_type string
+}
+
+func (r *DeliveriesRepository) GetAll(ctx context.Context, filters PaginationFilters) ([]*entity.Delivery, error) {
+	const (
+		operation = "Repository.DeliveriesRepository.GetDeliveries"
+	)
+
+	offset := defaultLimit * (filters.Page - 1)
+	args := []interface{}{filters.Page, offset}
+
+	query := fmt.Sprintf(`
+		SELECT 
+				count(*) OVER() AS total_count,
+				id,
+				reference,
+				qty,
+				created_at
+		FROM delivery
+		ORDER BY %s %s
+		LIMIT $1 OFFSET $2;	
+	`, filters.Sort_by, filters.Sort_type)
+
+	rows, err := r.Client.Pool.Query(
+		ctx,
+		query,
+		args...,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	totalRecords := 0
+	deliveries := []*entity.Delivery{}
+
+	for rows.Next() {
+		var delivery entity.Delivery
+
+		err := rows.Scan(
+			&totalRecords,
+			&delivery.ID,
+			&delivery.Reference,
+			&delivery.Qty,
+			&delivery.CreatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		deliveries = append(deliveries, &delivery)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("%s -> %w", operation, err)
+	}
+
+	print("Total: ", totalRecords)
+
+	return deliveries, nil
+}

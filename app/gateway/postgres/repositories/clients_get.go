4cfb38e0bb4b237c61bb36f03b71c8f6e4456026
dev-gst <141775462+dev-gst@users.noreply.github.com>
2024-06-29 22:12:15 -0300

    Lista de clientes (#9)
    * Create client entity
    
    * Implement client usecase, handler, repository and migration
    
    * Implement sorting and pagination
    
    * Remove unnecessary print to console
    
    * Add newline
    
    * Fix errors presented by lint
    
    * Remove unnecessary comment
    
    * Change func name in client_handler.go to ListClients
    
    * Add schema for client handler
    
    * Remove whitespace to make lint happy
    
    * Refactor client schema
    
    ---------
    
    Co-authored-by: o-mago <alexcabral.mago@gmail.com>
    Co-authored-by: Leonardo Müller <71836831+leonardo-gmuller@users.noreply.github.com>

---
+++ app/gateway/postgres/repositories/clients_get.go
@@ -0,0 +1,45 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/techhub-jf/farmacia-back/app/domain/entity"
+	"github.com/techhub-jf/farmacia-back/app/gateway/api/handler/schema"
+)
+
+const getClientsClause = `
+SELECT
+id,
+reference,
+full_name,
+cpf,
+rg,
+phone,
+created_at
+FROM client
+WHERE deleted_at IS NULL
+ORDER BY %s %s
+LIMIT $1 OFFSET $2
+`
+
+func (r *ClientsRepository) GetClients(
+	ctx context.Context, cqp schema.ClientQueryParams) (
+	[]*entity.Client, error,
+) {
+	const operation = "Repository.ClientsRepository.GetClients"
+
+	finalQuery := fmt.Sprintf(getClientsClause, cqp.SortBy, cqp.SortType)
+
+	offset := (cqp.Page - 1) * cqp.Limit
+	rows, _ := r.Pool.Query(ctx, finalQuery, cqp.Limit, offset)
+
+	clients, err := pgx.CollectRows(rows, pgx.RowToAddrOfStructByNameLax[entity.Client])
+	if err != nil {
+		return []*entity.Client{}, fmt.Errorf("%s -> %w", operation, err)
+	}
+
+	return clients, nil
+}
